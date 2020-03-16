3ca72f2fb99cf71579ebb3858939e89052ddebc3
KimYeonJun <duswns1783@naver.com>
2020-03-16 17:45:04 +0900

    ResponseDto 테스트 추가

--- src/test/java/com/webservice/springboot/web/HelloControllerTest.java
+++ src/test/java/com/webservice/springboot/web/HelloControllerTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class) //테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킨다.
                             //여기서는 SpringRunner라는 스프링 실행자를 사용.
@@ -28,5 +28,25 @@ public class HelloControllerTest {
                 .andExpect(status().isOk()) //HTTP Header의 Status를 검증한다. 여기 OK는 200인지 아닌지를 검증한다.
                 .andExpect(content().string(hello)); //mvc.perform의 결과를 검증. Controller에서 "hello"를 리턴하기 때문에
                                                     //이 값이 맞는지 검증한다.
+    }
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+        mvc.perform(
+                    get("/hello/dto")
+                        .param("name",name) //API 테스트할 때 사용되 요청 파라미터를 설정한다.
+                        .param("amount",String.valueOf(amount))) //"amount"라는 파라미터에 String.valueof(amount)를 담아서 보낸다.
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name))) //JSON응답값을 필드별로 검증할 수 있는 메소드
+                .andExpect(jsonPath("$.amount",is(amount)));
+        /*
+        get("/hello/dto")의 겨과로 HelloResponseDto가 반환이 되는데, HelloREsponseDto의 필드인 name과, amount를 검증해야 하므로
+        .andExpect(jsonPath("$.name"
+        .andExpect(jsonPath("$.amount"    가 들어간 것이다.
+        만약 HelloResponsDto의 필드가 namename과 amountamount 라면
+        .andExpect(jsonPath("$.namename"
+        .andExpect(jsonPaht("$.amountamount"가 들어가야함.
+         */
     }
 }
