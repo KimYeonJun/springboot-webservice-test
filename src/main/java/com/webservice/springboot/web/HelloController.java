0e41de6ff3bd6fd55917ea527980c96bc7daa444
KimYeonJun <duswns1783@naver.com>
2020-03-16 17:44:11 +0900

    ResponseDto 추가

--- src/main/java/com/webservice/springboot/web/HelloController.java
+++ src/main/java/com/webservice/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.webservice.springboot.web;
 
+import com.webservice.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 준다.
@@ -9,6 +11,12 @@ public class HelloController {
                           //  /hello로 요청이 오면 hello를 반환하는 기능을 가진다.
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, //"name"이란 이름으로 넘긴 파라미터를 String name에 저장.
+                                     @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 
 }
